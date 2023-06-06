758e5f92a7b0e982d1d84767085f18df7c3e3d61
Michael <michaeljohneduave@gmail.com>
2023-06-05 23:09:15 -0700

    Wrap greeting insdie useEffect WIP bedtime component

--- src/app/components/Greeting.tsx
+++ src/app/components/Greeting.tsx
@@ -1,25 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useGetFromAuth } from "../hooks/zustand";
 import useAuth from "../stores/useAuth";
+import Bedtime from "./Bedtime";
 
 const dayjs = require("dayjs");
 
 export default function Greeting() {
   const name = useGetFromAuth(useAuth, (state) => state.name);
-  const fname = name?.split(" ").slice(0, -1).join(" ");
-  const now = dayjs();
-  let greeting = `Good morning, ${fname}`;
+  const [greeting, setGreeting] = useState("");
 
-  if (now.format("A") === "PM" && now.format("HH") > 18) {
-    greeting = `Good afternoon, ${fname}`;
-  } else if (now.format("A") === "PM") {
-    greeting = `Good evening, ${fname}`;
-  }
+  useEffect(() => {
+    const now = dayjs();
+    const fname = name?.split(" ").slice(0, -1).join(" ");
+    let greeting = `Good morning, ${fname}`;
+
+    if (now.format("A") === "PM" && now.format("HH") > 18) {
+      greeting = `Good afternoon, ${fname}`;
+    } else if (now.format("A") === "PM") {
+      greeting = `Good evening, ${fname}`;
+    }
+
+    setGreeting(greeting);
+  }, [name]);
 
   return (
     <div>
       <h1 className="text-4xl">{greeting}!</h1>
+      <Bedtime />
     </div>
   );
 }
