af599f21a34a43d596a0d53cfa7d4e049e17046a
Michael <michaeljohneduave@gmail.com>
2023-06-09 23:36:53 -0700

    WIP: add habit

--- src/app/components/AddHabit.tsx
+++ src/app/components/AddHabit.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "./Modal";
 import { Plus, XSquare } from "lucide-react";
 import { Input } from "./ui/input";
@@ -8,11 +8,55 @@ import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Toggle } from "./ui/toggle";
+import { Textarea } from "./ui/textarea";
 
-const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] as const;
+type Day = (typeof DAYS)[number];
 
 export default function AddHabit() {
   const [showModal, setShowModal] = useState(false);
+  const [days, setDays] = useState<string[]>([]);
+  const [multi, setMulti] = useState<"everyday" | "weekdays" | null>(null);
+
+  useEffect(() => {
+    if (days.length === DAYS.length) {
+      setMulti("everyday");
+    } else if (
+      days.length === 5 &&
+      days.filter((d) => d !== "Sat" && d !== "Sun").length === 5
+    ) {
+      setMulti("weekdays");
+    } else {
+      setMulti(null);
+    }
+  }, [days.length]);
+
+  useEffect(() => {
+    if (!showModal) {
+      setDays([]);
+    }
+  }, [showModal]);
+
+  const handleMultiPress = (
+    isPressed: boolean,
+    type: "everyday" | "weekdays"
+  ) => {
+    if (!isPressed) {
+      setDays([]);
+    } else if (type === "everyday") {
+      setDays([...DAYS]);
+    } else if (type === "weekdays") {
+      setDays([...DAYS.filter((d) => d !== "Sat" && d !== "Sun")]);
+    }
+  };
+
+  const handleDayPress = (isPressed: boolean, day: Day) => {
+    if (isPressed) {
+      setDays([...days, day]);
+    } else {
+      setDays([...days.filter((d) => d !== day)]);
+    }
+  };
 
   return (
     <div className="w-full flex">
@@ -25,7 +69,7 @@ export default function AddHabit() {
       </Button>
       {showModal ? (
         <Modal>
-          <div className="bg-white rounded shadow-lg w-1/3">
+          <div className="bg-white rounded-2xl shadow-lg w-1/3">
             <ModalHeader>
               <div className="flex items-center px-10 py-4">
                 <span>Add Habit</span>
@@ -49,7 +93,11 @@ export default function AddHabit() {
                   />
                 </div>
                 <div className="space-y-3">
-                  <span className="text-lg">2. Type</span>
+                  <span className="text-lg">2. Details</span>
+                  <Textarea name="details" placeholder="Add habit details" />
+                </div>
+                <div className="space-y-3">
+                  <span className="text-lg">3. Type</span>
                   <RadioGroup className="flex" defaultValue="todo">
                     <div className="flex items-center space-x-2">
                       <RadioGroupItem value="todo" id="todo" />
@@ -62,19 +110,41 @@ export default function AddHabit() {
                   </RadioGroup>
                 </div>
                 <div className="space-y-3">
-                  <span className="text-lg">3. Frequency</span>
+                  <span className="text-lg">4. Frequency</span>
                   <div className="flex space-x-2">
-                    {days.map((day) => (
-                      <Toggle className="flex-1" variant="outline" key={day}>
+                    {DAYS.map((day) => (
+                      <Toggle
+                        className="flex-1"
+                        variant="outline"
+                        key={day}
+                        pressed={days.includes(day)}
+                        onPressedChange={(isPressed) =>
+                          handleDayPress(isPressed, day)
+                        }
+                      >
                         {day}
                       </Toggle>
                     ))}
                   </div>
                   <div className="flex space-x-2">
-                    <Toggle className="flex-1" variant="outline">
+                    <Toggle
+                      className="flex-1"
+                      variant="outline"
+                      onPressedChange={(isPressed) =>
+                        handleMultiPress(isPressed, "weekdays")
+                      }
+                      pressed={multi === "weekdays"}
+                    >
                       Week Days
                     </Toggle>
-                    <Toggle className="flex-1" variant="outline">
+                    <Toggle
+                      className="flex-1"
+                      variant="outline"
+                      onPressedChange={(isPressed) =>
+                        handleMultiPress(isPressed, "everyday")
+                      }
+                      pressed={multi === "everyday"}
+                    >
                       Every Day
                     </Toggle>
                   </div>
@@ -82,7 +152,7 @@ export default function AddHabit() {
               </div>
             </ModalBody>
             <ModalFooter>
-              <div className="flex items-center space-x-2 py-4 px-10">
+              <div className="flex items-center space-x-10 py-4 px-10">
                 <Button className="flex-1" variant="default">
                   Add
                 </Button>
