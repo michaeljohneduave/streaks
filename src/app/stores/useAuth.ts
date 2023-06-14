0ea50487e30b18a111b4a49544329fdf1b93f949
Michael <michaeljohneduave@gmail.com>
2023-06-13 23:46:00 -0700

    Remove zustand state persistence in localstorage

--- src/app/stores/useAuth.ts
+++ src/app/stores/useAuth.ts
@@ -1,53 +1,90 @@
+import {
+  getAuth,
+  onAuthStateChanged,
+  browserLocalPersistence,
+} from "firebase/auth";
 import { create } from "zustand";
-import { persist, PersistStorage } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-import { mergeDeepLeft } from "ramda";
+import app from "@/firebase/config";
 
 type State = {
   email: string;
   name: string;
   photoURL: string;
   isLoggedIn: boolean;
+  isLoggingIn: boolean;
 };
 
 type Actions = {
   setEmail: (email: string) => void;
+  setLoggingIn: () => void;
   setLogin: (email: string, name: string, photoURL: string) => void;
   setLogout: () => void;
 };
 
-export default create(
-  persist(
-    immer<State & Actions>((set) => ({
+const auth = getAuth(app);
+
+auth.setPersistence(browserLocalPersistence);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("user", user);
+    store.setState({
+      email: user.email || "",
+      name: user.displayName || "",
+      photoURL: user.photoURL || "",
+      isLoggedIn: true,
+    });
+  } else {
+    store.setState({
       email: "",
       name: "",
       photoURL: "",
       isLoggedIn: false,
+    });
+  }
+});
 
-      setEmail(email: string) {
-        set((state) => {
-          state.email = email;
-        });
-      },
-      setLogin(email, name, photoURL) {
-        set((state) => {
-          state.email = email;
-          state.name = name;
-          state.photoURL = photoURL;
-          state.isLoggedIn = true;
-        });
-      },
-      setLogout() {
-        set((state) => {
-          state.email = "";
-          state.name = "";
-          state.photoURL = "";
-          state.isLoggedIn = false;
-        });
-      },
-    })),
-    {
-      name: "auth",
-    }
-  )
+const store = create(
+  immer<State & Actions>((set) => ({
+    email: "",
+    name: "",
+    photoURL: "",
+    isLoggedIn: false,
+    isLoggingIn: false,
+
+    setEmail(email: string) {
+      set((state) => {
+        state.email = email;
+      });
+    },
+
+    setLoggingIn() {
+      set((state) => {
+        state.isLoggingIn = true;
+      });
+    },
+
+    setLogin(email, name, photoURL) {
+      set((state) => {
+        state.email = email;
+        state.name = name;
+        state.photoURL = photoURL;
+        state.isLoggedIn = true;
+        state.isLoggingIn = false;
+      });
+    },
+    setLogout() {
+      auth.signOut();
+
+      set((state) => {
+        state.email = "";
+        state.name = "";
+        state.photoURL = "";
+        state.isLoggedIn = false;
+      });
+    },
+  }))
 );
+
+export default store;
