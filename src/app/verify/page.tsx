53489d4efbc1c6b4217152db70a3a0f0de359fd4
Michael <michaeljohneduave@gmail.com>
2023-06-15 12:59:24 -0700

    Use correct file

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signInPasswordlessConfirm } from "@/firebase/auth/signin";
+import { signInPasswordlessConfirm } from "@/firebase/auth";
 import { useEffect, useState } from "react";
 import { Loader2 } from "lucide-react";
 
