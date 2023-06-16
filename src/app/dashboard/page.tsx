373df468de7003e75847a106a9c8c3f74ef05afa
Michael <michaeljohneduave@gmail.com>
2023-06-16 16:48:37 -0700

    Redirect non authorized user to home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,26 @@
+import { getServerSession } from "next-auth";
 import Greeting from "../components/Greeting";
 import HabitBoard from "../components/HabitBoard";
 import HabitList from "../components/HabitList";
+import WeekGrid from "../components/WeekGrid";
+import WeekProgress from "../components/WeekProgress";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <>
       <main className="container mx-auto py-10 px-28">
         <div className="flex space-x-10">
           <div className="flex flex-col basis-[70%] space-y-5">
             <Greeting />
-            <HabitBoard />
+            <HabitBoard progress={<WeekProgress />} gridd={<WeekGrid />} />
           </div>
           <div className="basis-[30%]">
             <HabitList />
