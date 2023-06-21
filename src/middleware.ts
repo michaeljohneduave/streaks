108fc8353d6962616af57fe7faeed9e3aed54dfa
Michael <michaeljohneduave@gmail.com>
2023-06-21 01:08:29 -0700

    Add table names to schema Add prisma client Minor changes to middleware Add prisma migrations in gitignore

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
-import { withAuth } from "next-auth/middleware";
-import { NextRequest, NextResponse } from "next/server";
-
 export const config = {
   matcher: ["/dashboard"],
 };
 
-export default withAuth(function middleware(req: NextRequest) {
-  return NextResponse.next();
-});
+export { default } from "next-auth/middleware";
